b1a52a34b83989e8efabf0fa6c878b7fb04b6208
jootnet <shawryan@outlook.com>
2023-08-20 12:22:36 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: "./src/JootM2.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "JootM2.js",
+		library: 'JootM2',
+        libraryTarget: 'var'
+    },
+	module: {
+        rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin()
+    ]
+};
